e0999d40e781e044dcc881a7d4ba7080b87c01b3
Ananth <ananthsoft@gmail.com>
2023-09-14 02:41:13 -0700

    Applying multiple styles using css module using join

--- react-app/src/components/ListGroup/ListGroup.tsx
+++ react-app/src/components/ListGroup/ListGroup.tsx
@@ -14,7 +14,7 @@ function ListGroup({ items, heading, onSelectedItem }: listGroupProps) {
   return (
     <>
       <h1>{heading}</h1>
-      <ul className={styles.listGroup}>
+      <ul className={[styles.listGroup, styles.container].join(" ")}>
         {items.map((item, index) => (
           <li
             key={item}
