924d4c511f03d33ebfc4e14ba03bb60275a44fc2
Ananth <ananthsoft@gmail.com>
2023-09-18 00:37:22 -0700

    Object can be updated using immer produce function

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Button from "./components/Button/Button";
 import Like from "./components/Like/Like";
 import Message from "./components/Message";
+import produce from "immer";
 
 function App() {
   const [bugs, setBugs] = useState([
@@ -11,8 +12,16 @@ function App() {
 
   const handleUpdateClick = () => {
     // Updating the array object
+    // setBugs(
+    //   bugs.map((bug) => (bug.bugId === 2 ? { ...bug, fixed: true } : bug))
+    // );
+
+    // updating the object using immer produce function
     setBugs(
-      bugs.map((bug) => (bug.bugId === 2 ? { ...bug, fixed: true } : bug))
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.bugId == 1);
+        if (bug) bug.fixed = true;
+      })
     );
   };
 
