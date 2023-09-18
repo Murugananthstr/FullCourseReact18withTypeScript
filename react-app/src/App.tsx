2679a2419010da1fe92197ae349d270657e777a2
Ananth <ananthsoft@gmail.com>
2023-09-17 21:09:30 -0700

    demo on How to update nested object

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import Button from "./components/Button/Button";
 import Like from "./components/Like/Like";
 import Message from "./components/Message";
 
 function App() {
+  const [customer, setCustomer] = useState({
+    name: "Ananth",
+    Address: {
+      City: "Surrey",
+      ZipCode: "V3S 5G2",
+    },
+  });
+
+  const handleClick = () => {
+    setCustomer({
+      ...customer,
+      Address: { ...customer.Address, City: "Bellevue" },
+    });
+  };
   return (
     <div>
-      <Button
-        children="My Button"
-        color="primary"
-        onClick={() => console.log("Button Clicked")}
-      />
-      <Like onClick={(e) => console.log(e)} />
-      <Message />
-      <Message />
+      <pre>{JSON.stringify(customer, null, 2)}</pre>
+
+      <button onClick={handleClick}>
+        Click Here to Update the Customer object
+      </button>
     </div>
   );
 }
