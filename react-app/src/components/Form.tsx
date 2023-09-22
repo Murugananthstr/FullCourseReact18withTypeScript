b26b4535f430b7bde05d9c71edabe68771b444fb
Ananth <ananthsoft@gmail.com>
2023-09-22 01:26:12 -0700

    accessing inputs from the form controls

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
+  const [person, setPerson] = useState({ name: "", age: 0 });
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (nameRef.current) person.name = nameRef.current.value;
+    if (ageRef.current) person.age = parseInt(ageRef.current.value);
+    console.log(person);
+  };
+
   return (
-    <form
-      onClick={() =>
-        console.log(
-          "Demo how the form submission is reloding the Form after submitting"
-        )
-      }
-    >
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="from-label">
           Name :
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input id="name" type="text" ref={nameRef} className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="Age" className="for-label">
           Age :
         </label>
-        <input id="Age" type="number" className="form-control" />
+        <input id="Age" ref={ageRef} type="number" className="form-control" />
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
